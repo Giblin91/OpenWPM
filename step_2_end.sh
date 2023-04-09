6860b90d286483ffbb64b035ad117e89f83863a4
Giblin91 <casinilorenzo91@gmail.com>
2023-04-09 22:21:06 +0200

    update to step_2

--- step_2_end.sh
+++ step_2_end.sh
@@ -6,6 +6,9 @@ echo "STEP 2 - START"
 echo "Activating conda environment"
 echo '----------------------------------------------------------'
 echo '----------------------------------------------------------'
+
+# Make conda available to shell script
+eval "$(conda shell.bash hook)"
 conda activate openwpm
 
 echo "Updating"
