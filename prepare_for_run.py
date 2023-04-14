4568eb8ab22ae94e14429fd839610df3bd136290
Giblin91 <casinilorenzo91@gmail.com>
2023-04-14 11:16:20 +0200

    fix to prepare for run script

--- prepare_for_run.py
+++ prepare_for_run.py
@@ -1,5 +1,5 @@
 from custom.File_Helper import (check_create_dir, check_remove_dir, check_remove_file,
-                                ROOT, DATADIR, D_EXTRACT, LEVELDB, OWPM_LOG, SQLITE)
+                                ROOT, DATADIR, D_EXTRACT, LEVELDB, OWPM_LOG, SQLITE, DCFP_LOG)
 
 
 resp = input("This step will clean all existing crawl data. Do you really wish to continue? [y/n]\n")
@@ -15,6 +15,7 @@ if resp.casefold() == "y":
         check_remove_dir(LEVELDB)
         check_remove_dir(D_EXTRACT)
         check_remove_file(OWPM_LOG)
+        check_remove_file(DCFP_LOG)
         check_remove_file(SQLITE)
         check_remove_file(DATADIR / "crawl-data.sqlite-journal")
     else:
